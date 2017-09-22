70c6fbdef9605d813789eb07108fa78e96c78874
Maciej Sawicki <sawik11@wp.pl>
2017-09-22 23:22:32 +0200

    Added junt

---
+++ src/test/kotlin/pl/humberd/youtube/PlaylistDownloaderTest.kt
@@ -0,0 +1,13 @@
+package pl.humberd.youtube
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class PlaylistDownloaderTest {
+    @Test
+    fun getPlaylistService() {
+        
+    }
+
+}
