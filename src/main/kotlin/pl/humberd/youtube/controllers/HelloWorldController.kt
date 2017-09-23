046f169ae72d892b39fc23f298dafa6f7c46cda5
Maciej Sawicki <sawik11@wp.pl>
2017-09-23 15:40:46 +0200

    Response formatting

--- src/main/kotlin/pl/humberd/youtube/controllers/HelloWorldController.kt
+++ src/main/kotlin/pl/humberd/youtube/controllers/HelloWorldController.kt
@@ -1,8 +1,6 @@
 package pl.humberd.youtube.controllers
 
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController()
